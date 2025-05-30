42b348f4830f6b982927e7c628ea5a7a148fab11
PandorAura <aurapandor@gmail.com>
2025-05-30 03:26:11 +0300

    modify url

--- adoption-site/my-app/src/services/AnimalService.ts
+++ adoption-site/my-app/src/services/AnimalService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://pet-finder-be-production.up.railway.app';
+const API_BASE_URL = 'https://pet-finder-be-production.up.railway.app/api';
 
 export interface Animal {
   id: number;
